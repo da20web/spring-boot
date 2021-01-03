97a1cded72194132e11993dea374870f16e19546
Da20web <rpdavi@hotmail.com>
2021-01-03 12:32:30 -0300

    Correção de pacotes não usados (CrossOrigin)

--- src/main/java/io/github/da20web/clientes/rest/ServicoPrestadoController.java
+++ src/main/java/io/github/da20web/clientes/rest/ServicoPrestadoController.java
@@ -7,7 +7,6 @@ import java.util.List;
 import javax.validation.Valid;
 
 import org.springframework.http.HttpStatus;
-import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
